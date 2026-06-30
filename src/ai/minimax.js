d45133bce0410b5563186947fba4d37980c24522
Codevek <vivekmjm8@gmail.com>
2026-06-30 23:25:43 +0530

    Refactor minimax and findBestMove functions to improve move evaluation and implement alpha-beta pruning. - nd now we can play the entire game with the AI like (u vs bot) in the terminal with full working rules and bestMoves

--- src/ai/minimax.js
+++ src/ai/minimax.js
@@ -1,65 +1,103 @@
 import { Chess } from "../core/game.js";
 import { evaluate } from "./evaluate.js";
 
-export function findBestMove(game) {
+export function findBestMove(game, depth) {
   const moves = game.getAllLegalMoves();
   let bestMove = null;
-  let bestScore = -Infinity;
   let score;
+  let bestScore
 
-  for (const move of moves) {
-    game.makeMove(move);
-    score = evaluate(game);
-    game.undoMove();
-
-    if (score > bestScore) {
-      bestMove = move;
-      bestScore = score;
-    }
-    // console.log(score, bestMove); 
+  if (game.getTurn() === "w") {
+    bestScore = -Infinity;
+  } else {
+    bestScore = Infinity;
   }
-  console.log(bestMove);
-  console.log(score);
+  if (game.getTurn() === "w") {
+    for (const move of moves) {
+      game.makeMove(move);
+      score = minimax(game, depth - 1, -Infinity, Infinity);
+      game.undoMove();
+
+      if (score > bestScore) {
+        bestMove = move;
+        bestScore = score;
+      }
+      // console.log(score, bestMove);
+    }
+  } else {
+    for (const move of moves) {
+      game.makeMove(move);
+      score = minimax(game, depth - 1, -Infinity, Infinity);
+      game.undoMove();
+
+      if (score < bestScore) {
+        bestMove = move;
+        bestScore = score;
+      }
+    }
+  }
+  // console.log(bestMove);
+  // console.log(score);
 
   return bestMove;
 }
 
+export function minimax(game, depth, alpha, beta) {
+  if (depth === 0) {
+    return evaluate(game);
+  }
 
-export function minimax(game, depth){
-  if(depth === 0){
-    return evaluate(game)
-  }
-  
-  const moves = game.getAllLegalMoves()
-  
-  if(game.getTurn() === "w"){
-    let bestScore = -Infinity
-    for(const move of moves){
-      game.makeMove(move)
-      let score = minimax(game, depth-1)
-      game.undoMove()
-      
-      bestScore = Math.max(bestScore, score)
+  const moves = game.getAllLegalMoves();
+  // console.log(moves);
+  if (moves.length === 0) {
+    if (game.isKingInCheck(game.getTurn())) {
+      const CHECKMATE = 100000;
+      if (game.getTurn() === "w") return -CHECKMATE + depth;
+      return CHECKMATE - depth;
     }
-    console.log(bestScore);
-    
-    return bestScore
+    return 0;
   }
-  else{
-    let bestScore = Infinity
-    for(const move of moves){
-      game.makeMove(move)
-      let score = minimax(game, depth-1)
-      game.undoMove()
-      
-      bestScore = Math.min(bestScore, score)
+
+  if (game.getTurn() === "w") {
+    let bestScore = -Infinity;
+    for (const move of moves) {
+      game.makeMove(move);
+      let score = minimax(game, depth - 1, alpha, beta);
+      game.undoMove();
+
+      bestScore = Math.max(bestScore, score);
+      alpha = Math.max(alpha, bestScore);
+
+      if (beta <= alpha) {
+        break;
+      }
+      // console.log(move);
     }
-    console.log(bestScore);
-    
-    return bestScore
+    // console.log(bestScore);
+
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (const move of moves) {
+      game.makeMove(move);
+      let score = minimax(game, depth - 1, alpha, beta);
+      game.undoMove();
+
+      bestScore = Math.min(bestScore, score);
+      beta = Math.min(beta, bestScore);
+
+      if (beta <= alpha) {
+        break;
+      }
+      // console.log(move);
+    }
+    // console.log(bestScore);
+    // console.log(totalMovesCalculated);
+
+    return bestScore;
   }
-  
 }
 
-const game = new Chess("4k3/8/8/8/8/8/3q4/3QK3 w - - 0 1");
-minimax(game, 2);
+// const game = new Chess("4k3/8/8/8/8/3q4/8/3K4 w - - 0 1");
+// console.table(game.getBoard());
+// console.log(findBestMove(game, 5));
--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
+import { findBestMove } from "./ai/minimax.js";
 import { generateFEN } from "./core/fen.js";
 import { Chess } from "./core/game.js";
 import readline from "node:readline/promises";
 
 const game = new Chess();
 
+// // game.makeMove({ from: [ 1, 6 ], to: [ 3, 4 ] })
+// console.table(game.getBoard());
+// console.log(findBestMove(game, 3));
+
 for (let i = 0; i < 50; i++) {
   console.table(game.getBoard());
-  const moves = game.getAllLegalMoves();
-  console.log(moves);
+  if (game.getTurn() === "w") {
+    const moves = game.getAllLegalMoves();
+    console.log(moves);
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  const answer = await rl.question("Make ur move?");
-  game.makeMove(moves[answer]);
-  console.log(generateFEN(game));
-  rl.close(); // Remember to close the stream to exit the process
+    const answer = await rl.question("Make ur move?");
+    game.makeMove(moves[answer]);
+    // console.log(generateFEN(game));
+    rl.close(); // Remember to close the stream to exit the process
+  }
+  else{
+   const move = findBestMove(game, 3)
+   game.makeMove(move)
+  }
 }
